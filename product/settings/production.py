d5db1063729b193f1d36a50ee24dacd69eb9622c
Szymon Cader <szymon.SC.cader@gmail.com>
2019-05-11 22:12:28 +0200

    remote db added to settings

--- product/settings/production.py
+++ product/settings/production.py
@@ -7,13 +7,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-        # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'db_name',
-    #     'USER': 'db_user',
-    #     'PASSWORD': 'db_password',
-    #     'HOST': 'db_host',
-    # }
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'product',
+        'USER': 'product',
+        'PASSWORD': '12SDdv@7F?lJ',
+        'HOST': 'localhost',
+    }
 
 }
 MEDIA_ROOT = '/var/www/product/media/'
